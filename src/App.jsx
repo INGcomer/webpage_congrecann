e0006eb24d217e0e4098016bf359e8acb269cb80
augusto antonelli <augustoantonelli88@gmail.com>
2023-12-04 18:03:49 -0300

    contador terminado

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <NavBar/>
       <Header/>
 
-      {/* <Contador/> */}
+      <Contador/>
 
       {/* <TextoIntermedio/> */}
 
--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -6,6 +6,9 @@ import Clock from "./Clock";
 import './Contador.css'
 // imgs
 import papel from './img/papel.png'
+import textura from './img/textura papel madera.jpg'
+import planta from './img/planta.png'
+import planta2 from './img/planta2.png'
 
 const Contador = () => {
   let deadline = "April, 19, 2024";
@@ -19,6 +22,13 @@ const Contador = () => {
             </div>
             <Clock deadline={deadline}/>
         </div>
+        <img src={textura} alt="Textura de papel" className='textura' />
+        <img src={planta} alt="planta 1" className='planta_1' />
+        <img src={planta} alt="planta 2" className='planta_2' />
+        <img src={planta2} alt="planta 3" className='planta_3' />
+        <img src={planta2} alt="planta 4" className='planta_4' />
+        <img src={planta2} alt="planta 5" className='planta_5' />
+        <img src={planta2} alt="planta 6" className='planta_6' />
     </section>
   );
 };
