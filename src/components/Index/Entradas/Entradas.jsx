c494409b8c7d0c0ee115cd9943f0b6c8f95dd560
augusto antonelli <augustoantonelli88@gmail.com>
2024-02-19 18:38:05 -0300

    cambio precios de entradas y timer actualizado

--- src/components/Index/Entradas/Entradas.jsx
+++ src/components/Index/Entradas/Entradas.jsx
@@ -9,7 +9,7 @@ import './Entradas.css';
 
 
 export default function Entradas() {
-    let deadline = "February, 1, 2024, 16:00:00";
+    let deadline = "February, 29, 2024, 16:20:00";
 
     return (
         <section id='Entradas'>
@@ -20,8 +20,8 @@ export default function Entradas() {
                     <p>
                     Sumérgete en el vibrante mundo del cannabis. Accede a todos los rincones de la Expo MendoCann 2024, desde las últimas novedades en tecnología cannábica hasta los shows en vivo más emocionantes. Una experiencia completa para todos los amantes del cannabis
                     </p>
-                    <h4>PRECIO POR DIA: $5.000</h4>
-                    <h4>PACK 2 DIAS: $8.000</h4>
+                    <h4>PRECIO POR DIA: $8.500</h4>
+                    <h4>PACK 2 DIAS: $13.500</h4>
 
                     {/* <Button>
                         COMPRAR
@@ -36,8 +36,8 @@ export default function Entradas() {
                     <p>
                     Da un paso hacia el futuro de los negocios cannábicos con nuestra Entrada B2B. Obtén acceso exclusivo a la Plataforma B2B y a espacios diseñados para conexiones empresariales. Participa en reuniones estratégicas, establece colaboraciones y descubre oportunidades únicas en la industria.
                     </p>
-                    <h4>PRECIO POR DIA: proximamente</h4>
-                    <h4>PACK 2 DIAS: proximamente</h4>
+                    <h4>PRECIO POR DIA: $17.000</h4>
+                    <h4>PACK 2 DIAS: $28.000</h4>
 
                     {/* <Button>
                         COMPRAR
@@ -50,8 +50,8 @@ export default function Entradas() {
                 <div className="entrada">
                     <h3> Entrada VIP </h3>
                     <p>Vive la experiencia definitiva con nuestra Entrada VIP. Disfruta de todos los beneficios de la Entrada B2B, además de acceso a una jornada completa de degustaciones, almuerzos exclusivos y eventos VIP. Sumérgete en la cultura cannábica de manera única y celebra con estilo en la Expo MendoCann 2024.</p>
-                    <h4>PRECIO POR DIA: $15.000</h4>
-                    <h4>PACK 2 DIAS: $25.000</h4>
+                    <h4>PRECIO POR DIA: $30.000</h4>
+                    <h4>PACK 2 DIAS: $50.000</h4>
 
                     {/* <Button>
                         COMPRAR
@@ -63,8 +63,8 @@ export default function Entradas() {
                 </div>
             </div>
 
-            {/* <a  className="comprar disabled"> <span>Comprar disponible en:</span> <span><Clock deadline={deadline}/></span>  </a> */}
-            <a href="https://www.showstickets.ar/event/mendocann" className="comprar"> <span>Comprar entradas</span>  </a>
+            <a  className="comprar disabled"> <span>Compra disponible en:</span> <span><Clock deadline={deadline}/></span>  </a>
+            {/* <a href="https://www.showstickets.ar/event/mendocann" className="comprar"> <span>Comprar entradas</span>  </a> */}
             
 
         </section>
