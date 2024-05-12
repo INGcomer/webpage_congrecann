54e182d5f1c0f4471672326d10f2579d8088d046
augusto antonelli <augustoantonelli88@gmail.com>
2024-05-12 01:36:38 -0300

    perfil listo

--- src/components/B2B/componentes/Perfil/Perfil.jsx
+++ src/components/B2B/componentes/Perfil/Perfil.jsx
@@ -2,6 +2,10 @@
 import React, { useState, useEffect } from 'react';
 // Axios
 import axios from "axios";
+// router
+import { useNavigate } from "react-router-dom";
+// cookies
+import Cookies from 'universal-cookie';
 // functions
 import { BackendURL } from '../../../../Config';
 // componentes
@@ -11,22 +15,77 @@ import './Perfil.css';
 
 
 export default function PerfilScreen() {
+    // cookies
+    const cookies = new Cookies();
+    // navigation
+    const navigate = useNavigate();
+    // data
+    const [UserData, SetUserData] = useState();
+    const [UserToken, SetUserToken] = useState(cookies.get('codigo'));
 
+    console.log(UserToken)
+
+    useEffect(() => {
+        axios({
+            method: 'post',
+            // url: 'http://192.168.0.14:3000/MatchAle/GetPerfil',
+            url: BackendURL('/MatchAle/GetPerfil'),
+            data: { codigo: `${UserToken}` },
+            headers: {
+                "Accept": "application/json"
+            },
+        }).then(function (response) {
+
+            SetUserData(response.data)
+
+            console.log(response.data)
+
+        }).catch(function (error) {
+
+            // Alert.alert('Pucha :(', 'No encontramos el codigo ingresado');
+
+            console.log(error);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!UserToken) {
+            // redirijo al usuario
+            navigate("/B2B/LogIn")
+        }
+    }, [UserToken]);
 
 
     return (
         <div className="Perfil">
-            <div className="data">
-                <div className="FotoContainer">
-                    <img src="" alt="" />
-                </div>
+            {UserData ?
+                <>
+                    <div className="data">
+                        <div className="FotoContainer">
+                            <img src={ BackendURL(`/imgs/MatchAle/${UserData.usuario.foto}.png`)} alt="" />
+                        </div>
 
-                <h1> nombre </h1>
-            </div>
+                        <h1> {UserData.usuario.nombre} </h1>
 
-            <Button>
-                Cerrar cecion
-            </Button>
+                        {/* <span>
+                        <h2> {UserData.usuario.ron_empresa} </h2>
+                        <h3> En: {UserData.empresa.nombre} </h3>
+                        </span> */}
+                        
+                    </div>
+
+
+                    <button onClick={() => {
+                        cookies.remove('codigo')
+                        SetUserToken(null)
+                        console.log('llega')
+                    }}>
+                        <Button > Cerrar sesión </Button>
+                    </button>
+                </>
+                :
+                null
+            }
         </div>
     )
 }
