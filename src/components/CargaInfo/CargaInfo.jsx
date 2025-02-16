8cec496d0ff9a0e8ace375fac5d9953beb95131d
augusto <augustoantonelli88@gmail.com>
2025-02-16 01:18:14 -0300

    avanzo con el form de carga de info

--- src/components/CargaInfo/CargaInfo.jsx
+++ src/components/CargaInfo/CargaInfo.jsx
@@ -91,12 +91,85 @@ export default function CargaInfo() {
 }
 
 function FomrsDeInfo({ CssTextField }) {
+    // Form
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    // Alerts
+    const MySwal = withReactContent(Swal)
 
+    // Envio de actividad de cronograma
     const onSubmitCronograma = data => {
-        console.log(data)
+        // Abro una pantalla de carga
+        MySwal.fire({
+            didOpen: () => { Swal.showLoading() }
+        })
+
+        // Envio informacion al backend
+        axios({
+            method: 'post',
+            url: 'http://192.168.0.101:3000/MatchAle/SaveActividad',
+            data: data.actividad
+        }).then(Response => {
+            MySwal.fire({         // si no ocurrio algun error muestro este mensaje
+                title: <strong> Informacion Guardada </strong>,
+                icon: 'success'
+            })
+
+            console.log(Response)
+        }).catch(error => {
+            MySwal.fire({         // si ocurrio algun error muestro este mensaje
+                title: <strong> ${error} </strong>,
+                icon: 'error'
+            })
+        })
     }
     const onSubmitDisertante = data => {
+        // Abro una pantalla de carga
+        MySwal.fire({
+            didOpen: () => { Swal.showLoading() }
+        })
+
+        // Envio informacion al backend
+        axios({
+            method: 'post',
+            url: 'http://192.168.0.101:3000/MatchAle/SaveActividad',
+            data: data.Disertante
+        }).then(Response => {
+            MySwal.fire({         // si no ocurrio algun error muestro este mensaje
+                title: <strong> Informacion Guardada </strong>,
+                icon: 'success'
+            })
+
+            console.log(Response)
+        }).catch(error => {
+            MySwal.fire({         // si ocurrio algun error muestro este mensaje
+                title: <strong> ${error} </strong>,
+                icon: 'error'
+            })
+        })
+    }
+    const onSubmitExpositor = data => {
+        let puntaje = 0
+        // Defino el puntaje
+        if (data.sponsor == 'plata') {
+            puntaje += 1
+        }
+        if (data.sponsor == 'oro') {
+            puntaje += 2
+        }
+        if (data.sponsor == 'diamante') {
+            puntaje += 3
+        }
+        if (data.stand == 'plata') {
+            puntaje += 1
+        }
+        if (data.stand == 'oro') {
+            puntaje += 2
+        }
+        if (data.stand == 'diamante') {
+            puntaje += 3
+        }
+
+        data.puntaje = puntaje
         console.log(data)
     }
     return (
@@ -108,56 +181,56 @@ function FomrsDeInfo({ CssTextField }) {
                     id="outlined-required"
                     label="Dia en numero 11 o 12"
                     name="Dia en numero 11 o 12"
-                    {...register("dia")}
+                    {...register("actividad.dia")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Nombre de la actividad"
                     name="Nombre de la actividad"
-                    {...register("nombre")}
+                    {...register("actividad.nombre")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Tipo de Actividad"
                     name="Tipo de Actividad"
-                    {...register("tipo")}
+                    {...register("actividad.tipo")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Es Medicinal, Industrial o de Cultivo"
                     name="Donde se realiza la actividad?"
-                    {...register("sector")}
+                    {...register("actividad.sector")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Descripcion de la actividad"
                     name="Descripcion de la actividad"
-                    {...register("descripcion")}
+                    {...register("actividad.descripcion")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Donde se realiza la actividad?"
                     name="Donde se realiza la actividad?"
-                    {...register("ubicacion")}
+                    {...register("actividad.ubicacion")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Hora de incio formato xx:xx"
                     name="Hora de incio formato xx:xx"
-                    {...register("inicio")}
+                    {...register("actividad.inicio")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Hora de fin formato xx:xx"
                     name="Hora de fin formato xx:xx"
-                    {...register("fin")}
+                    {...register("actividad.fin")}
                 />
                 <button type="submit">
                     <Button > Enviar Entrada de Cronograma </Button>
@@ -172,44 +245,51 @@ function FomrsDeInfo({ CssTextField }) {
                     id="outlined-required"
                     label="Nombre del disertante"
                     name="Nombre del disertante"
-                    {...register("nombre")}
+                    {...register("Disertante.nombre")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Descripcion del disertante"
                     name="Descripcion del disertante"
-                    {...register("descripcion")}
+                    {...register("Disertante.descripcion")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Foto"
+                    name="Foto"
+                    {...register("Disertante.foto")}
                 />
                 <CssTextField
                     id="outlined"
                     label="Link a la pagina web del disertante"
                     name="Link a la pagina web del disertante"
-                    {...register("web")}
+                    {...register("Disertante.web")}
                 />
                 <CssTextField
                     id="outlined"
                     label="link al instagram del disertante"
                     name="link al instagram del disertante"
-                    {...register("instagram")}
+                    {...register("Disertante.instagram")}
                 />
                 <CssTextField
                     id="outlined"
                     label="link al facebook del disertante"
                     name="link al facebook del disertante"
-                    {...register("facebook")}
+                    {...register("Disertante.facebook")}
                 />
                 <CssTextField
                     id="outlined"
                     label="link al X del disertante"
                     name="link al X del disertante"
-                    {...register("x")}
+                    {...register("Disertante.x")}
                 />
                 <CssTextField
                     id="outlined"
                     label="link al linkedin del disertante"
                     name="link al linkedin del disertante"
-                    {...register("linkedin")}
+                    {...register("Disertante.linkedin")}
                 />
 
 
@@ -221,28 +301,28 @@ function FomrsDeInfo({ CssTextField }) {
                     id="outlined-required"
                     label="Tipo de actividad"
                     name="Tipo de actividad"
-                    {...register("tipo")}
+                    {...register("Disertante.actividades[0].tipo")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Cuanto dura la actividad en formato xx minutos"
                     name="Cuanto dura la actividad en formato xx minutos"
-                    {...register("duracion")}
+                    {...register("Disertante.actividades[0].duracion")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Nombre de la actividad"
                     name="Nombre de la actividad"
-                    {...register("nombre")}
+                    {...register("Disertante.actividades[0].nombre")}
                 />
                 <CssTextField
                     required
                     id="outlined-required"
                     label="Descripcion de la actividad"
                     name="Descripcion de la actividad"
-                    {...register("descripcion")}
+                    {...register("Disertante.actividades[0].descripcion")}
                 />
 
                 <h4> Actividad 2 </h4>
@@ -250,29 +330,117 @@ function FomrsDeInfo({ CssTextField }) {
                     id="outlined"
                     label="Tipo de actividad"
                     name="Tipo de actividad"
-                    {...register("tipo")}
+                    {...register("Disertante.actividades[1].tipo")}
                 />
                 <CssTextField
                     id="outlined"
                     label="Cuanto dura la actividad en formato xx minutos"
                     name="Cuanto dura la actividad en formato xx minutos"
-                    {...register("duracion")}
+                    {...register("Disertante.actividades[1].duracion")}
                 />
                 <CssTextField
                     id="outlined"
                     label="Nombre de la actividad"
                     name="Nombre de la actividad"
-                    {...register("nombre")}
+                    {...register("Disertante.actividades[1].nombre")}
                 />
                 <CssTextField
                     id="outlined"
                     label="Descripcion de la actividad"
                     name="Descripcion de la actividad"
-                    {...register("descripcion")}
+                    {...register("Disertante.actividades[1].descripcion")}
                 />
 
                 <button type="submit">
                     <Button > Enviar Perfil de Disertante </Button>
+                </button>
+            </form>
+
+
+            <h2> Expositores </h2>
+            <form onSubmit={handleSubmit(onSubmitExpositor)}>
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Nombre del Expositor"
+                    name="Nombre del Expositor"
+                    {...register("Expositor.nombre")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Rubro del Expositor"
+                    name="Rubro del Expositor"
+                    {...register("Expositor.rubro")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Descripcion del Expositor"
+                    name="Descripcion del Expositor"
+                    {...register("Expositor.descripcion")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Foto"
+                    name="Foto"
+                    {...register("Expositor.foto")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Sector del Expositor"
+                    name="Sector del Expositor"
+                    {...register("Expositor.sector")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Tipo de sponsor, si tiene"
+                    name="Tipo de sponsor, si tiene"
+                    {...register("Expositor.sponsor")}
+                />
+                <CssTextField
+                    required
+                    id="outlined-required"
+                    label="Tipo de stand"
+                    name="Tipo de stand"
+                    {...register("Expositor.stand")}
+                />
+                <CssTextField
+                    id="outlined"
+                    label="Link a la pagina web del disertante"
+                    name="Link a la pagina web del disertante"
+                    {...register("Expositor.web")}
+                />
+                <CssTextField
+                    id="outlined"
+                    label="link al instagram del disertante"
+                    name="link al instagram del disertante"
+                    {...register("Expositor.instagram")}
+                />
+                <CssTextField
+                    id="outlined"
+                    label="link al facebook del disertante"
+                    name="link al facebook del disertante"
+                    {...register("Expositor.facebook")}
+                />
+                <CssTextField
+                    id="outlined"
+                    label="link al X del disertante"
+                    name="link al X del disertante"
+                    {...register("Expositor.x")}
+                />
+                <CssTextField
+                    id="outlined"
+                    label="link al linkedin del disertante"
+                    name="link al linkedin del disertante"
+                    {...register("Expositor.linkedin")}
+                />
+
+                <button type="submit">
+                    <Button > Enviar Perfil del Expositor </Button>
                 </button>
             </form>
         </>
