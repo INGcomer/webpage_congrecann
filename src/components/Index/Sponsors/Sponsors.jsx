b915219b652e2647c95506a1cffba88605627bde
augusto antonelli <augustoantonelli88@gmail.com>
2025-04-04 04:09:50 -0300

    agrego sponsors

--- src/components/Index/Sponsors/Sponsors.jsx
+++ src/components/Index/Sponsors/Sponsors.jsx
@@ -10,11 +10,15 @@ import { Pagination, Autoplay } from 'swiper/modules';
 // css
 import './Sponsors.css';
 // Sponsos
-import ingcome from './img/sponsors/ingcome.png'
-import StrechArt from './img/sponsors/ArtStrech.png'
-import Criolla from './img/sponsors/la criolla.png'
-import Multitudes from './img/sponsors/multitudes.png'
-import ohcho from './img/sponsors/ohcho blanco.png'
+import ARAHAND from './img/sponsors/ARAHAND.jpg';
+import CANNECT from './img/sponsors/CANNECT.png';
+import GROWCAST from './img/sponsors/GROWCAST.png';
+import HQA from './img/sponsors/HQA.png';
+import INSATIVA from './img/sponsors/INSATIVA.png';
+import PACU from './img/sponsors/PACCU.png';
+import KCB from './img/sponsors/KCBD.jpg';
+import UNIVERSAL from './img/sponsors/UNIVERSAL.png';
+
 
 export default function Sponsors() {
     const [CantidadSlides, SetCantidadSlides] = useState(2)
@@ -29,25 +33,48 @@ export default function Sponsors() {
         <section id='Sponsors'>
             <div className='sponsors nosAcompañan'>
                 <h2> Nos acompañan: </h2>
-                <img src="" alt="" />
-                <img src="" alt="" />
+                <img src="" alt="Gobierno de Mendoza" />
             </div>
             <div className='sponsors Diamante'>
-            <h2> Sponsors Diamante: </h2>
-
+                <h2> Sponsors Diamante: </h2>
+                <div className="imgs">
+                    <img src={ARAHAND} alt="Arahand Seeds" />
+                    <img src={KCB} alt="KCB Corp" />
+                    <img src={UNIVERSAL} alt="Universal Growing" />
+                </div>
             </div>
             <div className='sponsors oro'>
-            <h2> Sponsors Oro: </h2>
-
+                <h2> Sponsors Oro: </h2>
+                <div className="imgs">
+                    <img src="" alt="Green Fire" />
+                    <img src="" alt="CTH" />
+                    <img src={CANNECT} alt="Cannect" />
+                    <img src={PACU} alt="Laboratorios Paccu" />
+                    <img src="" alt="HarvestPro SA" />
+                    <img src="" alt="FECCA" />
+                    <img src="" alt="GdSF Estrategia y Estudio Jurídico" />
+                </div>
             </div>
             <div className='sponsors plata'>
-            <h2> Sponsors Plata: </h2>
+                <h2> Sponsors Plata: </h2>
+                <div className="imgs">
+                    <img src={INSATIVA} alt="Insativa" />
+                    <img src={GROWCAST} alt="Growcast" />
+                    <img src="" alt="Ojitos Rojos " />
+                    <img src="" alt="Stand de Clubes" />
+                    <img src="" alt="Dr Cultivo" />
+                    <img src="" alt="Seedstockers" />
+                    <img src="" alt="Te Hacemos Cultivar" />
+                    <img src={HQA} alt="Laboratorio HQA" />
+                </div>
             </div>
             <div className='sponsors mediaParthners'>
-            <h2> Media Partner: </h2>
-            </div>
-            <div className='sponsors organiza'>
-            <h2> Organiza: </h2>
+                <h2> Media Partner: </h2>
+                <div className="imgs">
+                    <img src="" alt="Pelagatos" />
+                    <img src="" alt="Marimba" />
+                    <img src="" alt="Sontrip" />
+                </div>
             </div>
         </section>
     );
